1a37773c867c1953d08b5afe6e803112b06a2070
Lugo Luciano Nahuel <lucianonahuel4528@gmail.com>
2020-09-25 23:08:40 -0300

    Terminamos la pantalla de inicio de programacion NO lineal

--- src/components/NoLinealProgramming/index.js
+++ src/components/NoLinealProgramming/index.js
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-import { Button, Jumbotron, Container, Row, Col } from "reactstrap";
+import { Button, Jumbotron, Container, Row, Col, UncontrolledTooltip} from "reactstrap";
+import './index.css'
 
-const NoLinealProgramming = () => {
-    return(
+import {BsFillInfoSquareFill} from "react-icons/bs"
+
+
+
+const StockProblems = () => {   
+  return(
     <Container fluid className="App">
-      <Row className="">
+      <Row>
         <Col xs={12} md={6} className="mx-auto my-5">
-          <Jumbotron className="">
-            <Row>
-              <h2>La Programación No-Lineal aún no está disponible.</h2>
+          <Jumbotron>
+            <Row className="justify-content-center">
+                <div className="text-center">
+                    <h2>Problemas de Programacion NO Lineal</h2>
+                    <h4>¿Qué tipos de problema desea resolver?</h4>
+                </div>
             </Row>
-            <Row>
-                <Link to='/home'><Button className="fluid-left">Volver</Button></Link>
+            <Row className="mt-3 mx-auto">
+              <Col>
+                 <Link to={"carpeta"} >
+                    <UncontrolledTooltip target='button'>Programación convexa</UncontrolledTooltip>
+                    <Button size='lg' outline color="success" id='button-'> Programación convexa</Button>
+                </Link>
+                <Link to={"js"} >
+                  <UncontrolledTooltip target='info-'>Más Información</UncontrolledTooltip>
+                  <BsFillInfoSquareFill id="info-" color='info' style={{color:'#17A2B8', marginLeft:10, fontSize:30}}></BsFillInfoSquareFill>
+                </Link>
+              </Col>
+            </Row>
+            <Row className="mt-3 mx-auto">
+              <Col>
+                 <Link to={"carpeta"} >
+                    <UncontrolledTooltip target='button'>Programación cuadrática</UncontrolledTooltip>
+                    <Button size='lg' outline color="success" id='button-'> Programación cuadrática</Button>
+                </Link>
+                <Link to={"js"} >
+                  <UncontrolledTooltip target='info-'>Más Información</UncontrolledTooltip>
+                  <BsFillInfoSquareFill id="info-" color='info' style={{color:'#17A2B8', marginLeft:10, fontSize:30}}></BsFillInfoSquareFill>
+                </Link>
+              </Col>
+            </Row>
+            <Row className="mt-3 mx-auto">
+              <Col>
+                 <Link to={"carpeta"} >
+                    <UncontrolledTooltip target='button'>Programación geométrica</UncontrolledTooltip>
+                    <Button size='lg' outline color="success" id='button-'> Programación geométrica</Button>
+                </Link>
+                <Link to={"js"} >
+                  <UncontrolledTooltip target='info-'>Más Información</UncontrolledTooltip>
+                  <BsFillInfoSquareFill id="info-" color='info' style={{color:'#17A2B8', marginLeft:10, fontSize:30}}></BsFillInfoSquareFill>
+                </Link>
+              </Col>
+            </Row>
+            <Row className="mt-3 mx-auto">
+              <Col>
+                 <Link to={"carpeta"} >
+                    <UncontrolledTooltip target='button'>Programación estocástica </UncontrolledTooltip>
+                    <Button size='lg' outline color="success" id='button-'> Programación estocástica </Button>
+                </Link>
+                <Link to={"js"} >
+                  <UncontrolledTooltip target='info-'>Más Información</UncontrolledTooltip>
+                  <BsFillInfoSquareFill id="info-" color='info' style={{color:'#17A2B8', marginLeft:10, fontSize:30}}></BsFillInfoSquareFill>
+                </Link>
+              </Col>
+            </Row>
+            <Row className="mt-3 mx-auto">
+              <Col>
+                 <Link to={"carpeta"} >
+                    <UncontrolledTooltip target='button'>Método de combinaciones </UncontrolledTooltip>
+                    <Button size='lg' outline color="success" id='button-'> Método de combinaciones </Button>
+                </Link>
+                <Link to={"js"} >
+                  <UncontrolledTooltip target='info-'>Más Información</UncontrolledTooltip>
+                  <BsFillInfoSquareFill id="info-" color='info' style={{color:'#17A2B8', marginLeft:10, fontSize:30}}></BsFillInfoSquareFill>
+                </Link>
+              </Col>
+            </Row>
+            <Row className="btn-volver justify-content-center">
+                <Link to='/home'><Button>Volver</Button></Link>
             </Row>
           </Jumbotron>
         </Col>
@@ -20,4 +88,4 @@ const NoLinealProgramming = () => {
     </Container>
     )
     };
-export default NoLinealProgramming;
+export default StockProblems;
