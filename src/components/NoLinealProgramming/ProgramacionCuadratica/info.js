45041c55681a3e45d3fe2346cc536f5196563fb8
Eric Stride <strideeric94@gmail.com>
2020-10-12 14:19:02 -0300

    Modificaciones en INFO de cuadratica
    Se agrego informacion de formulas, hipotesis y variables de info.js del modelo de programacion cuadratica

--- src/components/NoLinealProgramming/ProgramacionCuadratica/info.js
+++ src/components/NoLinealProgramming/ProgramacionCuadratica/info.js
@@ -7,7 +7,6 @@ import MathJax from "react-mathjax"
 
 
 const Info = () => {
-
     //DEFINIMOS ESTOS ESTADOS PARA MANEJAR LOS COLLAPSE
     //Para controlar el collapse de Hipotesis
     const [collapseHipotesis, setCollapseHipotesis] = useState(false);
@@ -38,7 +37,7 @@ const Info = () => {
           <Jumbotron>
             <Row className="justify-content-center">
                 <div>
-                    <h2>Modelo Wilson</h2><br></br>
+                    <h2>Programación Cuadratica</h2><br></br>
                 </div>
             </Row>
             
@@ -49,15 +48,116 @@ const Info = () => {
                     </Row>
                     <Collapse isOpen={collapseHipotesis} onEntered={onEnteredHipotesis} onExited={onExitedHipotesis}>
                         <ul className='lista'>
-                            <li>No tenemos en cuenta si contamos o no con los recursos financieros.</li>
-                            <li>No se permiten faltantes.</li>
-                            <li>Demanda constante y conocida.</li>
-                            <li>Resposición instantánea.</li>
-                            <li>Costo unitario de almacenamiento por unidad de tiempo 𝑐1, constante.</li>
-                            <li>Costo de preparacion 𝑘, constante.</li>
-                            <li>Costo unitario de producto 𝑏, constante.</li>
-                            <li>No existen otros costos.</li>
-                            <li>Al comienzo de cada periodo no hay stock ni pedidos insatisfechos.</li>
+                            <li>La siguiente función define una forma cuadrática. <MathJax.Provider><MathJax.Node formula={"X^{T}*D*X"}/></MathJax.Provider></li>
+                            <li>Se supone que la matriz D es simetrica y negativa definida.</li>
+                            <li>z es estrictamente concava. Por lo tanto, el espacio de soluciones es convexo.</li>
+                            <li>X, C, D, A, b, λ, U son matrices.</li>
+                            <ul>
+                                <li>
+                                    <MathJax.Provider>
+                                            <MathJax.Node formula={
+                                                String.raw`X=\begin{pmatrix}
+                                                x_{11} & x_{12} & \dots & x_{1n} 
+                                                \end{pmatrix}^{T}`
+                                            } />
+                                    </MathJax.Provider>                                   
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <MathJax.Provider>
+                                            <MathJax.Node formula={
+                                                String.raw`C=\begin{pmatrix}
+                                                c_{11} & c_{12} & \dots & c_{1n} 
+                                                \end{pmatrix}`
+                                            } />
+                                    </MathJax.Provider>                                   
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <MathJax.Provider>
+                                            <MathJax.Node formula={
+                                                String.raw`b=\begin{pmatrix}
+                                                b_{11} & b_{12} & \dots & b_{1n} 
+                                                \end{pmatrix}^{T}`
+                                            } />
+                                    </MathJax.Provider>                                   
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <MathJax.Provider>
+                                        <div>
+                                            <MathJax.Node formula={
+                                                String.raw`A=\begin{pmatrix}
+                                                a_{11} & a_{12} & \dots & a_{1n} \\ 
+                                                a_{21} & a_{22} & \dots & a_{2n} \\ 
+                                                \vdots & \vdots & \ddots & \vdots \\ 
+                                                a_{m1} & a_{m2} & \dots & a_{mn}
+                                                \end{pmatrix}`
+                                            } />
+                                        </div>
+                                    </MathJax.Provider>                                   
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <MathJax.Provider>
+                                        <div>
+                                            <MathJax.Node formula={
+                                                String.raw`D=\begin{pmatrix}
+                                                d_{11} & d_{12} & \dots & d_{1n} \\ 
+                                                d_{21} & d_{22} & \dots & d_{2n} \\ 
+                                                \vdots & \vdots & \ddots & \vdots \\ 
+                                                d_{m1} & d_{m2} & \dots & d_{mn}
+                                                \end{pmatrix}`
+                                            } />
+                                        </div>
+                                    </MathJax.Provider>                                   
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                <MathJax.Provider>
+                                    <div>
+                                        <MathJax.Node formula={
+                                            String.raw`\lambda=\begin{pmatrix}
+                                            \lambda_{11} & \lambda_{12} & \dots & \lambda_{1n} 
+                                            \end{pmatrix}^{T}`
+                                        } />
+                                    </div>
+                                </MathJax.Provider>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                <MathJax.Provider>
+                                    <div>
+                                        <MathJax.Node formula={
+                                            String.raw`U=\begin{pmatrix}
+                                            u_{11} & u_{12} & \dots & u_{1n} 
+                                            \end{pmatrix}^{T}`
+                                        }/>
+                                    </div>
+                                </MathJax.Provider>
+                                </li>
+                            </ul>
+                            <li>
+                                <MathJax.Provider>
+                                    <div>
+                                        <MathJax.Node formula={String.raw`A*X\leq b`} />
+                                    </div>
+                                </MathJax.Provider>
+                            </li>
+                            <li>
+                                <MathJax.Provider>
+                                    <div>
+                                        <MathJax.Node formula={String.raw`X\geq0`}/>
+                                    </div>
+                                </MathJax.Provider>
+                            </li>
+                            
                         </ul>
                     </Collapse>
                 </Card>
@@ -71,39 +171,64 @@ const Info = () => {
                 <Collapse isOpen={collapseFormulas} onEntered={onEnteredFormulas} onExited={onExitedFormulas}> 
                     <MathJax.Provider>
                         <div>
-                            <MathJax.Node formula={"Costo Total De Preparacion = \\frac{D}{q}*K"} />
+                            <MathJax.Node formula={"z=C*X+X^{T}*D*X"} />
+                        </div>
+                    </MathJax.Provider>
+
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={
+                                String.raw`G(X)=
+                                \begin{pmatrix}A \\ -I \\ \end{pmatrix}X-
+                                \begin{pmatrix}b \\ 0 \\ \end{pmatrix}
+                                \leq0`
+                            } />
                         </div>
                     </MathJax.Provider>
                     <MathJax.Provider>
                         <div>
-                            <MathJax.Node formula={"Costo Total Del Producto = b*D"} />
+                            <MathJax.Node formula={
+                                String.raw`\triangledown z - (\lambda^{T},U^{T})*\triangledown G(X)=0`
+                            } />
                         </div>
                     </MathJax.Provider>
                     <MathJax.Provider>
                         <div>
-                            <MathJax.Node formula={"Costo Total De Almacenamiento = \\frac{1}{2}*q*T*c1"} />
+                            <MathJax.Node formula={
+                                String.raw`\triangledown z = C+2*X^{T}*D`
+                            } />
                         </div>
                     </MathJax.Provider>
                     <MathJax.Provider>
                         <div>
-                            <MathJax.Node formula={"Costo Total Esperado = \\frac{D}{q}*K + b*D + \\frac{1}{2}*q*T*c1 "} />
-                        </div>
-                    </MathJax.Provider>    
-                    <MathJax.Provider>
-                        <div>
-                            <MathJax.Node formula={"Costo Total Esperado Optimo =  b*D + \\sqrt{2*T*D*K*c1} "} />
-                        </div>
-                    </MathJax.Provider>    
-                    <MathJax.Provider>
-                        <div>
-                            <MathJax.Node formula={"q_{0} = \\sqrt{\\frac{2*K*D}{T*c1}} "} />
-                        </div>
-                    </MathJax.Provider>    
-                    <MathJax.Provider>
-                        <div>
-                            <MathJax.Node formula={"To = \\frac{T}{n_{0}} = \\frac{T*q_{0}}{D} = \\sqrt{\\frac{2*K*T}{D*c1}} "} />
+                            <MathJax.Node formula={
+                                String.raw`\triangledown G(X)=
+                                \begin{pmatrix}A \\ -I \\ \end{pmatrix}`
+                            } />
                         </div>
                     </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={
+                                String.raw`S=b-A*X \geq 0 `
+                            } />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={
+                                String.raw`\begin{pmatrix}
+                                -2D & A^{T} & -I & 0 \\ A & 0 & 0 & I \\ 
+                                \end{pmatrix}
+                                \begin{pmatrix}
+                                X \\ \lambda \\ U \\ S
+                                \end{pmatrix} =
+                                \begin{pmatrix}
+                                C^{T} \\ b
+                                \end{pmatrix}`
+                            } />
+                        </div>
+                    </MathJax.Provider>      
                 </Collapse>    
                 </Card>
             </Row>
@@ -115,16 +240,13 @@ const Info = () => {
 
                     <Collapse isOpen={collapseVariables} onEntered={onEnteredVariables} onExited={onExitedVariables}>
                         <ul className='text-left'>
-                            <li><b>D: </b>Demanda</li>
-                            <li><b>K: </b>Costo de Preparación</li>
-                            <li><b>c1: </b>Costo de Almacenamiento</li>
-                            <li><b>b: </b>Costo Unitario del Producto</li>
-                            <li><b>q: </b>Lote Optimo</li>
-                            <li><b>t0: </b>Tiempo que dura el lote optimo antes de agotarse</li>
-                            <li><b>CTPrep: </b>Costo Total de Preparación</li>
-                            <li><b>CTProd: </b>Costo Total propio del producto</li>
-                            <li><b>CTA: </b>Costo Total de Almacenamiento</li>
-                            <li><b>CTE: </b>Costo Total Esperado</li>
+                            <li><b> C: </b> Matriz 1 </li>
+                            <li><b> D: </b> Matriz 2 </li>
+                            <li><b> A: </b> Matriz 3 </li>
+                            <li><b> b: </b> Numero </li>
+                            <li><b> S: </b> Variables de holgura de las restricciones </li>
+                            <li><b> λ: </b> Multiplicador de Lagrange </li>
+                            <li><b> U: </b> Multiplicador de lagrange </li>
                         </ul>
                     </Collapse>
                 </Card>
@@ -132,13 +254,13 @@ const Info = () => {
             <Row className="justify-content-left">
                 <Card body outline color="secondary">
                     <CardTitle><b>Bibliografía</b></CardTitle>
-                    <CardText>Claudio L. R. Sturla</CardText>
+                    <CardText>Hamdy A. Taha</CardText>
                 </Card>
             </Row>
             
             
             <Row className="btn-volver justify-content-center">
-                <Link to='/StockProblems'><Button>Volver</Button></Link>
+                <Link to='/NoLinealProgramming'><Button>Volver</Button></Link>
             </Row>
           </Jumbotron>
         </Col>
